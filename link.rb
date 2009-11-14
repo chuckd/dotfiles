58e3407521ad054a3fa39fb6efe10d3601178731
Charles Dale <charlie@aphid.net>
2009-11-14 11:16:56 +1100

    Make link.rb add git post-commit hook

--- link.rb
+++ link.rb
@@ -14,4 +14,8 @@ Dir.chdir File.dirname(__FILE__) do
       system %[ln -vsf #{File.join(dotfiles_dir, file)} #{target}]
     end
   end
+
+  `echo 'git push' > .git/hooks/post-commit`
+  `chmod 755 .git/hooks/post-commit`
 end
+
