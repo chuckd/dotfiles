bf879cd207a77d8f8d8f7642c874b6ed7fd6ba0d
Charles Dale <charlie@aphid.net>
2009-11-26 11:36:03 +1100

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,12 @@ if test (uname -s) = "Darwin"
   set fish_greeting ""
 
   set -x CDPATH . ~ ~/Sites
-  set -x GEM_HOME $GEM_PATH
-  set -x GEM_PATH $HOME/.gem/ruby/1.8
-  set -x PATH /usr/local/bin ~/.gem/ruby/1.8/bin /usr/bin /bin /usr/sbin /sbin /usr/X11/bin
+  set -x EDITOR mvim
+  set -x FLEX_HOME "/usr/local/flex"
+  set -x GIT_EDITOR "mvim -f"
+  set -x GEM_HOME $HOME/.gem/ruby/1.8
+  set -x GEM_PATH $GEM_HOME
+  set -x JAVA_HOME /Library/Java/Home
   set -x MANPATH /usr/local/share/man /usr/X11/man /usr/share/man
+  set -x PATH /usr/local/bin ~/.gem/ruby/1.8/bin /usr/bin /bin /usr/sbin /sbin /usr/X11/bin /usr/local/flex/bin
 end
